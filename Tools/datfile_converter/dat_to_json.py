913dd9499555fca1ce28e6e590d40a74254710ad
PauJuan <pambientoleg@gmail.com>
2023-11-04 16:28:41 +0000

    WIP reading dat file into dict

--- Tools/datfile_converter/dat_to_json.py
+++ Tools/datfile_converter/dat_to_json.py
@@ -2,57 +2,141 @@
 Python version of SIMCAT
 """
 
-# TODO Read DATFILE as text file, line by line
+from datfile import DatFile, config  # Main dict to contain data and config file
 
-# TODO Process all the sections:
-# [0] Metadata at the top of the file
-# [1] General - superseded
-# [2] Determinands - short name and units, linked to features
-# [3] Reaches - order (simno), id, name, waterbody, length, connectivity, velocity (alpha
-# and beta), temperature, EQS targets (per det), decay rates (per det)
-# [4] River Flow - dist type, params, linked to feature
-# [5] River Quality - per determinand, dist type and params, linked to feature
-# [6] Effluent Flow and Quality - flow and per det, dist type and
-# params, linked to feature
-# [7] River Quality Targets - superseded
-# [8] Intermittent Discharges - superseded
-# [9] Features - id, name, feat type, distance (km), coordinates (BNG)
 
-# TODO Parse data from each section into Reaches and Features (as a dict)
-dat_file = {
-        "metadata" : {},
-        "reaches": {
-            "reach_id" : {
-                "metadata" : {},
-                "features" : {
-                    "feature_id": {
-                        "metadata": {},
-                        "flow_params": {},
-                        "wq_params": {},
-                        }
-                    }
-                }
-            }
-        }
+def process_dat_file_lines(file_path="EXAMPLE.dat", config, line, dat_file):
+    """
+    Process all the sections:
+    [0] Metadata at the top of the file
+    [1] General - superseded
+    [2] Determinands - short name and units, linked to features
+    [3] Reaches - order (simno), id, name, waterbody, length, connectivity,
+    velocity (alpha and beta), temperature, EQS targets (per det), decay rates (per det)
+    [4] River Flow - dist type, params, linked to feature
+    [5] River Quality - per determinand, dist type and params, linked to feature
+    [6] Effluent Flow and Quality - flow and per det, dist type and params,
+    linked to feature
+    [7] River Quality Targets - superseded
+    [8] Intermittent Discharges - superseded
+    [9] Features - id, name, feat type, distance (km), coordinates (BNG)
+    """
+    # Open DAT file for reading
+    try:
+        with open(file_path, 'r') as file:
+            # Read the file line by line
+            line = next(file) # Skip first line
+            for line in file:
+                # Process each line and add data to json
+                # Use a different function for each section
+                # Metadata section
+                config.File.end:
+                process_metadata(line, dat_file)
 
-# TODO
+                # Determinands section
 
-# TODO
 
-# TODO
 
-# TODO
 
-# TODO
+    except FileNotFoundError:
+        print(f"The file '{file_path}' was not found.")
 
-# TODO
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-# TODO
 
-# TODO
 
-# TODO
+# Parse dat file metadata
+def process_metadata(line, dat_file):
+    """
+    """
+    pass
 
-# TODO
 
-# TODO
+# Parse determinand data (to populate decay and feature quality sections)
+def process_determinand_section(line, dat_file):
+    """
+    """
+    pass
+
+
+# Parse reaches
+def process_reaches_section(line, dat_file):
+    """
+    """
+    pass
+
+
+# Parse river flow and quality and eff flow and quality using dataset code as key
+def process_river_flow_section(line, dat_file):
+    """
+    """
+    pass
+
+
+def process_river_quality_section(line, dat_file):
+    """
+    """
+    pass
+
+
+def process_effluent_section(line, dat_file):
+    """
+    """
+    pass
+
+
+# Parse features and assing flow and quality data
+def process_features_section(line, dat_file):
+    """
+    """
+    pass
+
+
+# ChatGPT Solution
+# Define functions to process different sections
+def process_section1(lines):
+    # Implement logic for processing section 1
+    for line in lines:
+        print(f"Processing section 1: {line}")
+
+def process_section2(lines):
+    # Implement logic for processing section 2
+    for line in lines:
+        print(f"Processing section 2: {line}")
+
+# Open and read the text file
+with open("your_text_file.txt", "r") as file:
+    lines = file.readlines()
+
+# Initialize variables to track section boundaries
+section_start = None
+section_end = None
+
+# Iterate through the lines
+for line in lines:
+    if "START_SECTION1" in line:
+        section_start = "START_SECTION1"
+        section_lines = []
+    elif "END_SECTION1" in line:
+        section_end = "END_SECTION1"
+        if section_start and section_end:
+            process_section1(section_lines)
+            section_start = section_end = None
+    elif "START_SECTION2" in line:
+        section_start = "START_SECTION2"
+        section_lines = []
+    elif "END_SECTION2" in line:
+        section_end = "END_SECTION2"
+        if section_start and section_end:
+            process_section2(section_lines)
+            section_start = section_end = None
+    elif section_start:
+        section_lines.append(line)
+
+# Ensure that any remaining lines are processed
+if section_start:
+    if section_start == "START_SECTION1":
+        process_section1(section_lines)
+    elif section_start == "START_SECTION2":
+        process_section2(section_lines)
---
+++ Tools/datfile_converter/datfile.py
@@ -0,0 +1,73 @@
+"""
+The main object to hold the dat file data
+"""
+
+    # [3] Reaches - order (simno), id, name, waterbody, length, connectivity,
+    # velocity (alpha and beta), temperature, EQS targets (per det), decay rates (per det)
+    # [4] River Flow - dist type, params, linked to feature
+    # [5] River Quality - per determinand, dist type and params, linked to feature
+    # [6] Effluent Flow and Quality - flow and per det, dist type and params, linked to feature
+    # [7] River Quality Targets - superseded
+    # [8] Intermittent Discharges - superseded
+    # [9] Features - id, name, feat type, distance (km), coordinates (BNG)
+
+# Create data structures to hold the dat file info
+DatFile = {
+        "metadata" : {
+            "Flow Calibration Table": "",
+            "WQ Calibration Table": "",
+            "Regional Database Location": "",
+            "Common Database Location":  "",
+            "SAGIS Project Location": "",
+            "SAGIS Build No": "",
+            "Created by": "",
+            "DAT File Creation Date": "",
+            },
+        "reaches": {
+            # "reach_id": Reach
+            }
+        }
+
+# Reach = {
+#         "metadata": {},
+#         "features": {
+#             # "feature_id": Feature
+#             }
+#         }
+
+# Feature = {
+#         "metadata": {},
+#         "flow_params": {},
+#         "wq_params": {},
+#         }
+
+config = {
+        "File": {
+            "start": "=======================================================================",
+            "end": "======================================================================="
+            },
+        "Determinands": {
+            "start": "========b===========c======d====e====f=====g====h=====i===j====k==l===m=",
+            "end": "*************** indicator of end of the determinand data ***********"
+            },
+        "Reaches": {
+            "start": "=========b===================c====d====e====f====g====h===i===j=====",
+            "end": "***************** indicator of end of Reach data *******************"
+            },
+        "RiverFlow": {
+            "start": "===========c========d========e=========f============g===============",
+            "end": "***************** indicator of end of river flow data **************"
+            },
+        "RiverQuality": {
+            "start": "=======b=c====d======e=====f========g====h=======i==================",
+            "end": "***************** indicator of end of river quality data *********"
+            },
+        "Effluent": {
+            "start": "======b=c=========d========e=====f========g===h=====i==================",
+            "end": "************ indicator of end of effluent flow and quality data ******"
+            },
+        "Features": {
+            "start": "===========a=================================b====c====d=====e====f====g===h=i==j===",
+            "end": "***************** indicator of end of data *********"
+            }
+        }
